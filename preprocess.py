4d51f97482f34ef22c9edb12b697c7db735c7bd2
Piotr Binkowski <piotr.binkowski1@gmail.com>
2018-12-05 09:46:24 +0100

    add colored preprocessed images

--- preprocess.py
+++ preprocess.py
@@ -6,14 +6,16 @@ import os
 
 imsize = 64
 
-def preproc(fname, folder):
+def preproc(fname, folder, grayscale):
 	image = cv2.imread(fname)
 	if image is None:
 		print("Failed to open %s" % fname)
 		return
 	resized = cv2.resize(image, (imsize, imsize))
-	gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
-	cv2.imshow("image", gray)
+	if grayscale:
+		gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
+	else:
+		gray = resized
 	cv2.imwrite("./dataset/%s/%s" % (folder ,os.path.basename(fname)) ,gray)
 
 pathlib.Path("./dataset/cat").mkdir(parents=True, exist_ok=True)
@@ -22,10 +24,10 @@ pathlib.Path("./dataset/dog").mkdir(parents=True, exist_ok=True)
 print("Cats")
 
 for fname in glob.glob("./PetImages/Cat/*.jpg"):
-	preproc(fname, "cat")
+	preproc(fname, "cat", False)
 
 print("Dogs")
 
 for fname in glob.glob("./PetImages/Dog/*.jpg"):
-	preproc(fname, "dog")
+	preproc(fname, "dog", False)
 
