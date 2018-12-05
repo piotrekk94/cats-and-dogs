80e383368fb9b2a030323a829231a86a4fb4b452
Piotr Binkowski <piotr.binkowski1@gmail.com>
2018-12-05 22:46:37 +0100

    improved network

--- main.py
+++ main.py
@@ -1,55 +1,50 @@
-# TensorFlow and tf.keras
 import tensorflow as tf
 from tensorflow import keras
 
-# Helper libraries
 import numpy as np
-import matplotlib.pyplot as plt
 
-#load images
 import glob
 import cv2
 
-fashion_mnist = keras.datasets.fashion_mnist
+dog_files = np.asarray([cv2.imread(i) for i in glob.glob("dataset/dog/*.jpg")])
+cat_files = np.asarray([cv2.imread(i) for i in glob.glob("dataset/cat/*.jpg")])
 
-dog_files = np.asarray([cv2.imread(i, cv2.IMREAD_GRAYSCALE) for i in glob.glob("dataset/dog/*.jpg")])
-cat_files = np.asarray([cv2.imread(i, cv2.IMREAD_GRAYSCALE) for i in glob.glob("dataset/cat/*.jpg")])
-train_dog = dog_files[3000:]
-train_cat = cat_files[3000:]
-test_dog = dog_files[1000:3000]
-test_cat = cat_files[1000:3000]
+images_src = np.concatenate((dog_files, cat_files), axis=0)
+labels_src = np.concatenate((np.asarray([1 for i in dog_files]), np.asarray([0 for i in cat_files])), axis=0)
 
-train_images = np.concatenate((train_dog, train_cat), axis=0)
-train_labels = np.concatenate((np.asarray([1 for i in train_dog]), np.asarray([0 for i in train_cat])), axis=0)
-test_images = np.concatenate((test_dog, test_cat), axis=0)
-test_labels = np.concatenate((np.asarray([1 for i in test_dog]), np.asarray([0 for i in test_cat])), axis=0)
+images = images_src[3000:]
+labels = labels_src[3000:]
+
+test_img = images_src[1000:3000]
+test_labels = labels_src[1000:3000]
 
 class_names = ['cat', 'dog']
 
-train_images = train_images / 255.0
+images = images / 255.0
 
-test_images = test_images / 255.0
+print(keras.backend.image_data_format())
 
-plt.figure(figsize=(10,10))
-for i in range(25):
-    plt.subplot(5,5,i+1)
-    plt.xticks([])
-    plt.yticks([])
-    plt.grid(False)
-    plt.imshow(train_images[i], cmap=plt.cm.binary)
-    plt.xlabel(class_names[train_labels[i]])
-#plt.show()
+input_shape = (64, 64, 3)
+num_classes = 2
 
-model = keras.Sequential([
-    keras.layers.Flatten(input_shape=(64, 64)),
-    keras.layers.Dense(128, activation=tf.nn.relu),
-    keras.layers.Dense(2, activation=tf.nn.softmax)
-])
+model = keras.Sequential()
+
+model.add(keras.layers.Conv2D(32, kernel_size=(3, 3),
+                 activation='relu',
+                 input_shape=input_shape))
+model.add(keras.layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(keras.layers.MaxPooling2D(pool_size=(2, 2)))
+model.add(keras.layers.Dropout(0.25))
+model.add(keras.layers.Flatten())
+model.add(keras.layers.Dense(128, activation='relu'))
+model.add(keras.layers.Dropout(0.5))
+model.add(keras.layers.Dense(num_classes, activation='softmax'))
 
 model.compile(optimizer=tf.train.AdamOptimizer(),
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
-model.fit(train_images, train_labels, epochs=5)
-test_loss, test_acc = model.evaluate(test_images, test_labels)
+model.fit(images, labels, epochs=5, validation_split=0.2)
+
+test_loss, test_acc = model.evaluate(test_img, test_labels)
 
 print('Test accuracy:', test_acc)
